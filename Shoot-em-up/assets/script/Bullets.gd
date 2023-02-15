bad62d7bede72fbf7b715a8534172ffb446c9d3d
Poipurin <b.pinorojo@gmail.com>
2023-02-15 01:53:04 -0300

    player and bullets config

--- Shoot-em-up/assets/script/Bullets.gd
+++ Shoot-em-up/assets/script/Bullets.gd
@@ -1,35 +1,3 @@
 extends Area2D
 
-<<<<<<< HEAD
-export var speed = 400 
-var screen_size
 
-func _ready():
-	#screen_size = get_viewport_rect().size
-	hide()
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-=======
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
->>>>>>> 0c4a3e7acf76cab0d0d18413bc1755d5fc0292ba
