09127bd88a6337f13d90dbafc78ce99e1ebd7643
Poipurin <b.pinorojo@gmail.com>
2023-02-15 02:52:10 -0300

    player collisions

--- Shoot-em-up/assets/script/Player.gd
+++ Shoot-em-up/assets/script/Player.gd
@@ -1,14 +1,14 @@
 extends Area2D
-
+signal hit
 
 export var speed = 400 
 var screen_size
 
 func _ready():
-	#screen_size = get_viewport_rect().size
+	screen_size = get_viewport_rect().size
 	hide()
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	var velocity = Vector2.ZERO 
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	elif Input.is_action_pressed("move_left"):
@@ -35,3 +35,7 @@ func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
+
+
+func _on_Player_body_entered(body):
+	emit_signal("hit")
