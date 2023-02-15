182abf70f77f735f24effb4c75ce8f6b971e3362
Poipurin <b.pinorojo@gmail.com>
2023-02-15 01:35:52 -0300

    player and bullets config

---
+++ Shoot-em-up/assets/script/Bullets.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+export var speed = 400 
+var screen_size
+
+func _ready():
+	#screen_size = get_viewport_rect().size
+	hide()
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
