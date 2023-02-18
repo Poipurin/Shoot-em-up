34ff60d7844ce71f302ad32b611345e460c7dd28
Poipurin <b.pinorojo@gmail.com>
2023-02-18 19:29:02 -0300

    enemy test

--- Shoot-em-up/assets/scenes/Enemies.gd
+++ Shoot-em-up/assets/scenes/Enemies.gd
@@ -1,13 +1,22 @@
 extends KinematicBody2D
 
 var player = null
-
+var velocity = Vector2()
 export var speed = 2
+export var eDirection = 1
+
+enum animState{
+	attack,
+	death,
+	idle,
+	loadingAttack,
+}
+
+var anim_state = null
 
 func _ready():
-	$AnimatedSprite.playing = true
-	var mob_types = $AnimatedSprite.frames.get_animation_names()
-	#$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	anim_state = animState.idle
+	$AnimatedSprite.play("idle")
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
@@ -15,9 +24,25 @@ func _on_VisibilityNotifier2D_screen_exited():
 func _on_detection_body_entered(body):
 	if body.is_in_group("Player"):
 		player = body
+		
 func _physics_process(delta):
-	var movement = Vector2(-2, 0)
+	move_char()
 	
+func move_char():
+	velocity.y += 0
+	velocity.x = speed * eDirection
+	velocity = move_and_slide(velocity,Vector2.RIGHT)
 	if player:
-		movement = position.direction_to(player.position) * speed
-		movement = move_and_collide(movement)
+		anim_state = animState.loadingAttack
+		$AnimatedSprite.play("loadingAttack")
+	#	movement = position.direction_to(player.position) * speed
+	#	movement = move_and_collide(movement)
+
+
+
+func _on_AnimatedSprite_animation_finished():
+	if (anim_state == animState.loadingAttack):
+		anim_state = animState.attack
+		$AnimatedSprite.play("attack")
+	elif (anim_state == animState.attack):
+			_ready()
