a731901d489238340972843135d2f32976067937
Poipurin <b.pinorojo@gmail.com>
2023-02-18 17:35:15 -0300

    detection

--- Shoot-em-up/assets/scenes/Enemies.gd
+++ Shoot-em-up/assets/scenes/Enemies.gd
@@ -7,11 +7,17 @@ export var speed = 2
 func _ready():
 	$AnimatedSprite.playing = true
 	var mob_types = $AnimatedSprite.frames.get_animation_names()
-	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+	#$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 
-
+func _on_detection_body_entered(body):
+	if body.is_in_group("Player"):
+		player = body
 func _physics_process(delta):
 	var movement = Vector2(-2, 0)
+	
+	if player:
+		movement = position.direction_to(player.position) * speed
+		movement = move_and_collide(movement)
