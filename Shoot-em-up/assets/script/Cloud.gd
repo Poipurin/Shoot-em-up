c74b2c9b5e142fc2c2023b37728e173d8b3c6b26
Poipurin <b.pinorojo@gmail.com>
2023-02-20 18:29:04 -0300

    enemy animation

--- Shoot-em-up/assets/script/Cloud.gd
+++ Shoot-em-up/assets/script/Cloud.gd
@@ -3,13 +3,15 @@ extends KinematicBody2D
 var player = null
 var velocity = Vector2(0, 0)
 var speed = 32
-var gravity = 2
+var gravity = 0
+var screen_size
 
 func _ready():
 	$AnimationPlayer.play("idle")
-
-func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
+	screen_size = get_viewport_rect().size
+	
+#func _on_VisibilityNotifier2D_screen_exited():
+	#queue_free()
 
 func _on_detection_body_entered(body):
 	if body.is_in_group("Player"):
@@ -17,6 +19,7 @@ func _on_detection_body_entered(body):
 		
 func _process(delta):
 	move_character()
+	
 	
 func move_character():
 	velocity.x = -speed
--- Shoot-em-up/assets/script/Enemies.gd
+++ Shoot-em-up/assets/script/Enemies.gd
@@ -1,17 +1,6 @@
-extends KinematicBody2D
+extends Node2D
 
-var player = null
 
-export var speed = 2
 
 func _ready():
-	$AnimatedSprite.playing = true
-	var mob_types = $AnimatedSprite.frames.get_animation_names()
-	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
-
-func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
-
-
-func _physics_process(delta):
-	var movement = Vector2(-2, 0)
+	pass
