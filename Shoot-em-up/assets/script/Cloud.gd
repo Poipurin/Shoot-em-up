9e74f6a8a3f2f298d9661c3f22b6eea07dcd0d12
Poipurin <b.pinorojo@gmail.com>
2023-02-20 23:03:01 -0300

    almost functional enemy

--- Shoot-em-up/assets/script/Cloud.gd
+++ Shoot-em-up/assets/script/Cloud.gd
@@ -2,16 +2,17 @@ extends KinematicBody2D
 
 var player = null
 var velocity = Vector2(0, 0)
-var speed = 32
-var gravity = 0
+var speed = 40
 var screen_size
+
 
 func _ready():
 	$AnimationPlayer.play("idle")
+	velocity.x = +speed
 	screen_size = get_viewport_rect().size
 	
-#func _on_VisibilityNotifier2D_screen_exited():
-	#queue_free()
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
 
 func _on_detection_body_entered(body):
 	if body.is_in_group("Player"):
@@ -22,7 +23,10 @@ func _process(delta):
 	
 	
 func move_character():
-	velocity.x = -speed
-	velocity.y += gravity
+	if position.x > 220:
+		velocity.x = -speed
+	elif position.x < -210:
+		velocity.x = +speed
+	
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
